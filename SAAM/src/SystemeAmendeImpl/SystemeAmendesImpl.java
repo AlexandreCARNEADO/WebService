7355aca1c6ca4118fa50dd671d3ab2edf0bd4137
AlexandreCARNEADO <acarneado@yahoo.fr>
2017-01-25 18:01:34 +0100

    Ajout fonctions
    Enregistrer
    Signaler

--- SAAM/src/SystemeAmendeImpl/SystemeAmendesImpl.java
+++ SAAM/src/SystemeAmendeImpl/SystemeAmendesImpl.java
@@ -1,26 +1,47 @@
 package SystemeAmendeImpl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import SystemeAmendesInt.SystemeAmendes;
+import radars.Amende;
 import radars.Voiture;
 
 public class SystemeAmendesImpl implements SystemeAmendes {
 
+	Set<Voiture> listeVoitures;
+	Set<Amende> listeAmendes;
+	
+	public SystemeAmendesImpl(){
+		listeVoitures = new HashSet();
+		listeAmendes = new HashSet();
+	}
 	
 	@Override
 	public void enregistrer(Voiture v) {
-		// TODO Auto-generated method stub
-		
+		listeVoitures.add(v);		
 	}
 
 	@Override
-	public void signaler(String immatriculation, String modele, int tarif) {
-		// TODO Auto-generated method stub
+	public int signaler(String immatriculation, String modele, int tarif) {
 		
+		Amende a = null;
+		int numero = -1;
+		
+		for(Voiture v: listeVoitures){
+			if(v.getImmatriculation().equals(immatriculation)&& v.getModele().equals(modele)){
+				 a = new Amende(immatriculation,tarif);		
+				 numero = a.getNumero();
+			}
+		}	
+			return numero;		
 	}
 
 	@Override
-	public void lister(String immatriculation) {
-		// TODO Auto-generated method stub
+	public Amende[] lister(String immatriculation) {
+		
+		return null;
+		
 		
 	}
 
--- SAAM/src/SystemeAmendesInt/SystemeAmendes.java
+++ SAAM/src/SystemeAmendesInt/SystemeAmendes.java
@@ -1,16 +1,19 @@
 package SystemeAmendesInt;
 
+import java.util.Set;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
+import radars.Amende;
 import radars.Voiture;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface SystemeAmendes{
     @WebMethod void enregistrer(Voiture v);
-    @WebMethod void signaler(String immatriculation, String modele, int tarif);
-    @WebMethod void lister(String immatriculation);
+    @WebMethod int signaler(String immatriculation, String modele, int tarif);
+    @WebMethod Amende[] lister(String immatriculation);
     @WebMethod void payer(int numero, String nom, String prenom);
 }
